8a970c0a10bb74fe11b7d720b20dc2d5d2dae6f0
SpiritCroc <dev@spiritcroc.de>
2019-12-08 15:48:03 +0100

    Remove sony open device build barrier
    We want to use it on sony msm8974 as well, but enabling the sodp flag
    there will cause other build errors when having newer sony devices
    synced.
    
    Change-Id: Ice41dce7213f7d0ba6e6bd126e1170d03f708151

--- Android.mk
+++ Android.mk
@@ -1,5 +1,3 @@
-ifeq ($(PRODUCT_PLATFORM_SOD),true)
-
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -33,5 +31,3 @@ endif
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_EXECUTABLE)
-
-endif
